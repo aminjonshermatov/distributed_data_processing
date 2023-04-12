255a2dea8de74abaa269f71d7022d39c0b8b8fb4
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-12 22:55:49 +0300

    openmp/11

--- openmp/11.cpp
+++ openmp/11.cpp
@@ -9,6 +9,7 @@
 std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
 
 inline constexpr std::size_t N = 30u;
+inline constexpr std::int32_t DIVIDER = 9;
 
 using array_t = std::array<std::int32_t, N>;
 
@@ -16,14 +17,14 @@ int main() {
   auto array = array_t{};
   std::generate(array.begin(), array.end(), rnd);
 
-  std::uint32_t countMultiplesOf9 = 0u;
-#pragma omp parallel for num_threads(8) reduction(+: countMultiplesOf9)
+  std::uint32_t countMultiples = 0u;
+#pragma omp parallel for num_threads(8) reduction(+: countMultiples)
   for (std::size_t i = 0u; i < N; ++i) {
-    if (array[i] % 9 == 0) {
+    if (array[i] % DIVIDER == 0) {
 #pragma omp critical
-      ++countMultiplesOf9;
+      ++countMultiples;
     }
   }
 
-  std::cout << "Count of multiples of 9: " << countMultiplesOf9 << std::endl;
+  std::cout << "Count of multiples of " << DIVIDER << ": " << countMultiples << std::endl;
 }
