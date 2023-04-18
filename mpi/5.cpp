d6ca8f83022effa7efecdb86f8b9bbea01b07f47
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-18 22:29:54 +0300

    mpi/5

--- mpi/5.cpp
+++ mpi/5.cpp
@@ -1,11 +1,11 @@
 //
 // Created by aminjon on 4/18/23.
 //
-#include <iostream>
-#include <random>
 #include <chrono>
 #include <functional>
+#include <iostream>
 #include <mpi.h>
+#include <random>
 
 inline constexpr int N = 30;
 
@@ -17,12 +17,10 @@ inline constexpr int SEND_PARTIAL_TAG = 2;
 inline constexpr int LB = 1;
 inline constexpr int RB = 10;
 
-inline constexpr auto ninf = std::numeric_limits<int>::min();
-
 inline std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
 inline std::uniform_int_distribution<int> rnd_range(LB, RB);
 
-inline std::array<int, N> A{}, B{};
+inline std::array<int, N> A{}, B{}, recv_buf_A{}, recv_buf_B{};
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
@@ -32,8 +30,16 @@ int main(int argc, char *argv[]) {
   int cur_proc;
   MPI_Comm_rank(MPI_COMM_WORLD, &cur_proc);
 
+  const int per_proc = (N + n_procs - 1) / n_procs;
+  std::vector<int> send_cnt(n_procs, per_proc), offsets(n_procs);
+  for (int proc_id = 1; proc_id < n_procs; ++proc_id) {
+    offsets[proc_id] = per_proc * (proc_id - 1);
+  }
+  offsets[0] = (n_procs - 1) * per_proc;
+  send_cnt[0] = N - offsets[0];
+
   if (cur_proc == MASTER_PROC) {
-    [&](){
+    [&]() {
       const auto gen_print = [](std::array<int, N> &arr) {
         std::generate(arr.begin(), arr.end(), std::bind(rnd_range, std::ref(rnd)));
         std::cout << '[';
@@ -43,86 +49,55 @@ int main(int argc, char *argv[]) {
         }
         std::cout << ']' << std::endl;
       };
-      gen_print(A); gen_print(B);
+      gen_print(A);
+      gen_print(B);
     }();
+  }
 
-    const int per_proc = (N + n_procs - 1) / n_procs;
-    int used = 0;
-    for (int proc_id = 1; proc_id < n_procs; ++proc_id) {
-      MPI_Send(&per_proc,
-               1,
+  MPI_Scatterv(A.data(),
+               send_cnt.data(),
+               offsets.data(),
                MPI_INT,
-               proc_id,
-               SEND_LEN_TAG,
-               MPI_COMM_WORLD);
-      MPI_Send(A.data() + per_proc * (proc_id - 1),
-               per_proc,
+               recv_buf_A.data(),
+               N,
                MPI_INT,
-               proc_id,
-               SEND_ARRAY_PTR_TAG,
+               MASTER_PROC,
                MPI_COMM_WORLD);
-      MPI_Send(B.data() + per_proc * (proc_id - 1),
-               per_proc,
+  MPI_Scatterv(B.data(),
+               send_cnt.data(),
+               offsets.data(),
                MPI_INT,
-               proc_id,
-               SEND_ARRAY_PTR_TAG,
+               recv_buf_B.data(),
+               N,
+               MPI_INT,
+               MASTER_PROC,
                MPI_COMM_WORLD);
-      used += per_proc;
-    }
 
-    int64_t s = 0;
-    for (int i = used; i < N; ++i) {
-      s += A[i] * B[i];
-    }
+  int loc = 0;
+  for (int i = 0; i < send_cnt[cur_proc]; ++i) {
+    loc += recv_buf_A[i] * recv_buf_B[i];
+  }
 
-    for (int proc_id = 1; proc_id < n_procs; ++proc_id) {
-      int64_t partial;
+  int ans;
+  if (cur_proc == MASTER_PROC) {
+    ans = loc;
+    for (int i = 1; i < n_procs; ++i) {
+      int tmp;
       MPI_Status status;
-      MPI_Recv(&partial,
+      MPI_Recv(&tmp,
                1,
-               MPI_INT64_T,
+               MPI_INT,
                MPI_ANY_SOURCE,
                SEND_PARTIAL_TAG,
                MPI_COMM_WORLD,
                &status);
-      s += partial;
+      ans += tmp;
     }
-
-    std::cout << "Scalar: " << s << std::endl;
+    std::cout << "Scalar: " << ans << std::endl;
   } else {
-    int len;
-    std::array<int, N> tmpA{}, tmpB{};
-    MPI_Status status;
-    MPI_Recv(&len,
+    MPI_Send(&loc,
              1,
              MPI_INT,
-             MASTER_PROC,
-             SEND_LEN_TAG,
-             MPI_COMM_WORLD,
-             &status);
-    MPI_Recv(tmpA.data(),
-             len,
-             MPI_INT,
-             MASTER_PROC,
-             SEND_ARRAY_PTR_TAG,
-             MPI_COMM_WORLD,
-             &status);
-    MPI_Recv(tmpB.data(),
-             len,
-             MPI_INT,
-             MASTER_PROC,
-             SEND_ARRAY_PTR_TAG,
-             MPI_COMM_WORLD,
-             &status);
-
-    int64_t partial = 0;
-    for (int i = 0; i < len; ++i) {
-      partial += tmpA[i] * tmpB[i];
-    }
-
-    MPI_Send(&partial,
-             1,
-             MPI_INT64_T,
              MASTER_PROC,
              SEND_PARTIAL_TAG,
              MPI_COMM_WORLD);
